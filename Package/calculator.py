f6cf8e5eb6b5ef71c6b696c35e7c133df05bcfd1
Azk213 <2013.azimahmed@gmail.com>
2026-07-06 19:32:31 +0530

    Update

---
+++ Package/calculator.py
@@ -0,0 +1,24 @@
+#Substraction
+def sub(a,b):
+    return a-b
+
+#Addition
+def add(a,b)
+    return a+b
+
+#Multiplication
+def multiply(a,b)
+    return a*b
+
+#Division
+def div(a,b)
+    return a*b
+
+#Exponential
+def exp(a,b)
+    return a**b
+
+#Square
+def sqr(a)
+    return a*a
+
