564df5325f63981a850f26f1c553ab17ead56d75
Azk213 <2013.azimahmed@gmail.com>
2026-07-11 14:28:49 +0530

    Update

---
+++ Projects/pack/converter.py
@@ -0,0 +1,5 @@
+def miles_to_km(miles):
+    return miles * 1.60934
+
+def km_to_miles(km):
+    return km / 1.60934
---
+++ Projects/pack/math_tools.py
@@ -0,0 +1,40 @@
+# Subtraction
+def sub(a, b):
+    return a - b
+
+# Addition
+def add(a, b):
+    return a + b
+
+# Multiplication
+def multiply(a, b):
+    return a * b
+
+# Division
+def div(a, b):
+    return a / b
+
+# Exponential
+def exp(a, b):
+    return a ** b
+
+# Square
+def sqr(a):
+    return a * a
+
+# Area of circle with radius
+def areaofcircle_r(a):
+    return 3.14 * (a ** 2)
+
+# Area of circle with diameter
+def areaofcircle_d(a):
+    return 3.14 * ((a / 2) ** 2)
+
+# Even or Odd
+def odd_or_even(a):
+    if a % 2 == 0:
+        print(a, "is Even")
+    else:
+        print(a, "is Odd")
+    
+    
