84a9f284feeb0cfcff31cd728621ef920e9f70ba
Azk213 <2013.azimahmed@gmail.com>
2026-07-12 11:38:46 +0530

    Update

--- Practical Questions/Day_04.py
+++ Practical Questions/Day_04.py
@@ -1,1 +1,59 @@
-print("Nothing Given")
+print("________________________________________________________________________________")
+# Question 1
+fruits = ["apple", "banana", "orange", "strawberry", "kiwi"]
+print(fruits)
+
+print("________________________________________________________________________________")
+# Question 2
+colours = ["red", "yellow", "green", "blue", "purple"]
+print(colours[0], colours[2])
+
+print("________________________________________________________________________________")
+# Question 3
+numbers = ["1", "2", "3", "4", "5"]
+numbers.insert(1, 3)
+print(numbers)
+
+print("________________________________________________________________________________")
+# Question 4
+list = ["apple", "banana", "orange", "strawberry"]
+list.append("kiwi")
+print(list)
+
+print("________________________________________________________________________________")
+# Question 5
+list_2 = ["apple", "banana", "orange", "strawberry"]
+list_2.insert(1, "kiwi")
+print(list_2)
+
+print("________________________________________________________________________________")
+# Question 6
+list_3 = ["apple", "banana", "orange", "strawberry"]
+list_3.pop()
+print(list_3)
+
+list_3.remove("apple")
+print(list_3)
+
+print("________________________________________________________________________________")
+# Question 7
+list_4 = ["apple", "banana", "orange", "strawberry"]
+print(list_4[:2])
+
+print("________________________________________________________________________________")
+# Question 8
+list_5 = ["apple", "banana", "orange", "strawberry"]
+print(len(list_5))
+
+print("________________________________________________________________________________")
+# Question 9
+list_6 = [1, 2, 4, 3, 5]
+list_6.sort()
+print(list_6)
+
+print("________________________________________________________________________________")
+# Question 10
+list_a = ["apple", "banana"]
+list_b = ["orange", "strawberry"]
+list_a.extend(list_b)
+print(list_a)
--- Practical Questions/Day_06.py
+++ Practical Questions/Day_06.py
@@ -1,31 +1,41 @@
+print("________________________________________________________________________________")
 #Question 1
 numbers ={1, 2, 3, 4, 5}
 print(numbers)
+print("________________________________________________________________________________")
 #Question 2
 numbers.add(6)
 print(numbers)
+print("________________________________________________________________________________")
 #Question 3
 numbers.update({7,8,9})
 print(numbers)
+print("________________________________________________________________________________")
 #Question 4
 numbers.remove(8)
 print(numbers)
 numbers.discard(7)
 print(numbers)
+print("________________________________________________________________________________")
 #Question 5
 fruits ={"apple","mango","starberry"}
 fruits_1={"kiwi","melon"}
 print(fruits.union(fruits_1))
+print("________________________________________________________________________________")
 #Question 6
 print(fruits.intersection(fruits_1))
+print("________________________________________________________________________________")
 #Question 7
 print(fruits.difference(fruits_1))
+print("________________________________________________________________________________")
 #Question 8
 print(fruits.symmetric_difference(fruits_1))
+print("________________________________________________________________________________")
 #Question 9
 nums = [1,2,3,4,5,5,6,6,7,7,8,]
 print(nums)
 print(set(nums))
+print("________________________________________________________________________________")
 #Question 10
 alpha ={"a","b","c","d","e","f"}
 print(alpha.pop())
